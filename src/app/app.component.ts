2e830953e43cf32b0b5a266fc9372e3034973e01
Cliff Smith <cliff@hawkrdg.com>
2020-10-14 10:36:37 -0500

    1st commit after refactoring into single repo workspace...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
-export class AppComponent {
-  title = 'angularsynth';
+export class AppComponent implements OnInit {
+  title = 'WebAudio Synth';
+	audioCtx;
+	showHelp = false;
+	@ViewChild('synth1') synth1: ElementRef;
+	@ViewChild('synth2') synth2: ElementRef;
+
+	constructor(
+		private zone: NgZone
+	) {}
+
+	async ngOnInit() {
+		console.log('ngOnInit()...');
+		this.audioCtx = new (window as any).AudioContext();
+	}
+
+	async ngAfterViewInit() {
+	}
+
+	openHelp() {
+		this.showHelp = true;
+	}
+	closeHelp() {
+		this.showHelp = false;
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+// import { FormsModule } from '@angular/forms';
 
+
+// material imports...
+//
+import { MatButtonModule } from "@angular/material/button";
+// import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+// import { MatSliderModule } from '@angular/material/slider';
+// import { MatSelectModule } from '@angular/material/select';
+// import { MatInputModule } from '@angular/material/input';
+
+import { SynthModule } from "./synth/synth.module";
+// app components
+//
 import { AppComponent } from './app.component';
+// import { SynthComponent } from './synth/synth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // SynthComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+		CommonModule,
+		SynthModule,
+		// BrowserAnimationsModule,
+		// FormsModule,
+		MatButtonModule,
+		// MatSlideToggleModule,
+		// MatSliderModule,
+		// MatSelectModule,
+		// MatInputModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+		AppComponent
+	]
 })
 export class AppModule { }
